b437cad340c2494da17c049519fcaa141c2392ff
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-05 23:48:57 +0530

    fetch available places from backend...

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,25 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+   const [availablePlaces, setAvailablePlaces] = useState([]);
+
+   useEffect(() => {
+      fetch("http://localhost:3000/places")
+         .then((response) => {
+            return response.json();
+         })
+         .then((resData) => {
+            setAvailablePlaces(resData.places);
+         });
+   }, []);
+
+   return (
+      <Places
+         title="Available Places"
+         places={availablePlaces}
+         fallbackText="No places available."
+         onSelectPlace={onSelectPlace}
+      />
+   );
 }
