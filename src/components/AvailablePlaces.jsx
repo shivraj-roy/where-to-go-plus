60d77f0dd3ef1b08dc1f08b48f3c76103385bfb5
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-06 12:17:26 +0530

    feat: add error handling and ErrorPage component for fetching places...

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,20 +1,37 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import ErrorPage from "./ErrorPage.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
    const [availablePlaces, setAvailablePlaces] = useState([]);
    const [isFetching, setIsFetching] = useState(false);
+   const [error, setError] = useState();
 
    useEffect(() => {
       const fetchPlaces = async () => {
-         setIsFetching(true);
-         const response = await fetch("http://localhost:3000/places");
-         const resData = await response.json();
-         setAvailablePlaces(resData.places);
+         try {
+            setIsFetching(true);
+            const response = await fetch("http://localhost:3000/places");
+            const resData = await response.json();
+            setAvailablePlaces(resData.places);
+            if (!response.ok) {
+               throw new Error("Places could not be fetched...");
+            }
+         } catch (error) {
+            setError({
+               message: error.message || "Sorry!!! Something went wrong...",
+            });
+         }
          setIsFetching(false);
       };
       fetchPlaces();
    }, []);
+
+   if (error) {
+      return (
+         <ErrorPage title={"An error occurred..."} message={error.message} />
+      );
+   }
 
    return (
       <Places
