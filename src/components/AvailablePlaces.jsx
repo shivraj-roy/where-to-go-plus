3830c0884b32137b226e3611b8239618ffff3b9d
Shivraj Roy <shivrajroy78@gmail.com>
2025-03-06 13:15:24 +0530

    feat: refactor place fetching logic into a separate function...

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import ErrorPage from "./ErrorPage.jsx";
 import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
    const [availablePlaces, setAvailablePlaces] = useState([]);
@@ -12,14 +13,10 @@ export default function AvailablePlaces({ onSelectPlace }) {
       const fetchPlaces = async () => {
          try {
             setIsFetching(true);
-            const response = await fetch("http://localhost:3000/places");
-            const resData = await response.json();
-            if (!response.ok) {
-               throw new Error("Places could not be fetched...");
-            }
+            const places = await fetchAvailablePlaces();
             navigator.geolocation.getCurrentPosition((pos) => {
                const sortedPlaces = sortPlacesByDistance(
-                  resData.places,
+                  places,
                   pos.coords.latitude,
                   pos.coords.longitude
                );
